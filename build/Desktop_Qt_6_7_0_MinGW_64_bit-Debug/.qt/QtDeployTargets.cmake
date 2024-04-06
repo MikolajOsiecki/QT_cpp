b89b671ab11e9e7e4b4ef0ed2fd1337dacc74abf
MikolajOsiecki <mosiecki@student.agh.edu.pl>
2024-04-06 15:47:22 +0200

    Fix

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QT_cpp_FILE E:/AGH/Magisterka/QT_cpp/QT_cpp/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/QT_cpp.exe)
-set(__QT_DEPLOY_TARGET_QT_cpp_RUNTIME_DLLS E:/QT/6.7.0/mingw_64/bin/Qt6Widgets.dll;E:/QT/6.7.0/mingw_64/bin/Qt6Gui.dll;E:/QT/6.7.0/mingw_64/bin/Qt6Core.dll)
